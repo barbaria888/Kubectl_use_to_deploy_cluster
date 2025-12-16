a7cba63742ac081bf0338ec74848b712deda1f6a
HARDIK ARORA <arorahardik0811@gmail.com>
2025-12-16 21:45:45 +0530

    Replace kubectl commands with K3s setup scripts
    Updated commands for K3s installation and configuration.

--- commands.sh
+++ commands.sh
@@ -1,4 +1,14 @@
- kubectl create deployment my-deployment1 --image=nginx
-kubectl expose deployment my-deployment1 --port=80 --type=NodePort --name=my-service1
- kubectl get services
-kubectl get pods | awk 'awk NR>1 ${print 1}'
+curl -sfLk https://get.k3s.io | INSTALL_K3S_VERSION=v1.31.0+k3s1 K3S_TOKEN=KCNA INSTALL_K3S_EXEC="--disable traefik --kubelet-arg=eviction-hard=imagefs.available<1%,nodefs.available<1%" sh -
+ls -altrh /usr/local/bin/kubectl
+
+cat /etc/rancher/k3s/k3s.yaml
+
+mkdir -p ~/.kube; mv /etc/rancher/k3s/k3s.yaml ~/.kube/config
+
+ssh worker-1 'curl -sfLk https://get.k3s.io | INSTALL_K3S_VERSION=v1.31.0+k3s1 K3S_URL=https://control-plane:6443 K3S_TOKEN=KCNA INSTALL_K3S_EXEC="--kubelet-arg=eviction-hard=imagefs.available<1%,nodefs.available<1%" sh -'
+
+ssh worker-2 'curl -sfLk https://get.k3s.io | INSTALL_K3S_VERSION=v1.31.0+k3s1 K3S_URL=https://control-plane:6443 K3S_TOKEN=KCNA INSTALL_K3S_EXEC="--kubelet-arg=eviction-hard=imagefs.available<1%,nodefs.available<1%" sh -'
+
+alias k= 'kubectl'
+alias kgp= 'kubectl get pods'
+alias kgn= 'kubectl get nodes'
