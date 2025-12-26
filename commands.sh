0d30a836492b205c63ecd75921ae40f8a653d828
HARDIK ARORA <arorahardik0811@gmail.com>
2025-12-26 22:06:46 +0530

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,3 +1,7 @@
+alias k='kubectl'
+alias kgp='kubectl get pods'
+alias kgn='kubectl get nodes'
+
 curl -sfLk https://get.k3s.io | INSTALL_K3S_VERSION=v1.31.0+k3s1 K3S_TOKEN=KCNA INSTALL_K3S_EXEC="--disable traefik --kubelet-arg=eviction-hard=imagefs.available<1%,nodefs.available<1%" sh -
 ls -altrh /usr/local/bin/kubectl
 
@@ -9,6 +13,3 @@ ssh worker-1 'curl -sfLk https://get.k3s.io | INSTALL_K3S_VERSION=v1.31.0+k3s1 K
 
 ssh worker-2 'curl -sfLk https://get.k3s.io | INSTALL_K3S_VERSION=v1.31.0+k3s1 K3S_URL=https://control-plane:6443 K3S_TOKEN=KCNA INSTALL_K3S_EXEC="--kubelet-arg=eviction-hard=imagefs.available<1%,nodefs.available<1%" sh -'
 
-alias k='kubectl'
-alias kgp='kubectl get pods'
-alias kgn='kubectl get nodes'
