ebf5d99529e6ecfa191dd88103bafbb6f68bb7f1
HARDIK ARORA <arorahardik0811@gmail.com>
2026-06-25 21:09:41 +0530

    Update commands-single-node.sh

--- commands-single-node.sh
+++ commands-single-node.sh
@@ -22,22 +22,6 @@ mkdir -p ~/.kube
 mv /etc/rancher/k3s/k3s.yaml ~/.kube/config
 chmod 600 ~/.kube/config
 
-#-----------------------------------
-# Install K3s on Worker Nodes
-#-----------------------------------
-ssh -t worker-1 "curl -sfLk https://get.k3s.io | \
-INSTALL_K3S_VERSION=v1.31.0+k3s1 \
-K3S_URL=https://control-plane:6443 \
-K3S_TOKEN=KCNA \
-INSTALL_K3S_EXEC='--kubelet-arg=eviction-hard=imagefs.available<1%,nodefs.available<1%' \
-sh -"
-
-ssh -t worker-2 "curl -sfLk https://get.k3s.io | \
-INSTALL_K3S_VERSION=v1.31.0+k3s1 \
-K3S_URL=https://control-plane:6443 \
-K3S_TOKEN=KCNA \
-INSTALL_K3S_EXEC='--kubelet-arg=eviction-hard=imagefs.available<1%,nodefs.available<1%' \
-sh -"
 
 kgn
 kgp -A
