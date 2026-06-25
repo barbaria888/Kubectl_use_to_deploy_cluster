b1a8a6f15d58b767d84ad9fc15bc06dec28d8350
HARDIK ARORA <arorahardik0811@gmail.com>
2026-06-25 20:52:50 +0530

    Create commands-single-node.sh

---
+++ commands-single-node.sh
@@ -0,0 +1,43 @@
+#-----------------------------------
+# Aliases for convenience
+#-----------------------------------
+alias k='kubectl'
+alias kgp='kubectl get pods'
+alias kgn='kubectl get nodes'
+
+#-----------------------------------
+# Install K3s on Control Plane
+#-----------------------------------
+curl -sfL https://get.k3s.io | \
+INSTALL_K3S_VERSION=v1.31.0+k3s1 \
+K3S_TOKEN=KCNA \
+INSTALL_K3S_EXEC="--disable traefik --kubelet-arg=eviction-hard=imagefs.available<1%,nodefs.available<1%" \
+sh -
+
+# Verify kubectl installation
+ls -altrh /usr/local/bin/kubectl
+
+# Move K3s config to user's kube directory
+mkdir -p ~/.kube
+mv /etc/rancher/k3s/k3s.yaml ~/.kube/config
+chmod 600 ~/.kube/config
+
+#-----------------------------------
+# Install K3s on Worker Nodes
+#-----------------------------------
+ssh -t worker-1 "curl -sfLk https://get.k3s.io | \
+INSTALL_K3S_VERSION=v1.31.0+k3s1 \
+K3S_URL=https://control-plane:6443 \
+K3S_TOKEN=KCNA \
+INSTALL_K3S_EXEC='--kubelet-arg=eviction-hard=imagefs.available<1%,nodefs.available<1%' \
+sh -"
+
+ssh -t worker-2 "curl -sfLk https://get.k3s.io | \
+INSTALL_K3S_VERSION=v1.31.0+k3s1 \
+K3S_URL=https://control-plane:6443 \
+K3S_TOKEN=KCNA \
+INSTALL_K3S_EXEC='--kubelet-arg=eviction-hard=imagefs.available<1%,nodefs.available<1%' \
+sh -"
+
+kgn
+kgp -A
